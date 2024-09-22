4f6bc89d06f5ada51d76abe00ee6e020cb7bcf73
Shaun Phillips <smphillips@alumni.york.ac.uk>
2024-09-22 11:28:14 +0100

    add request mapping

--- src/main/java/csr/rest/GameController.java
+++ src/main/java/csr/rest/GameController.java
@@ -3,15 +3,17 @@ package csr.rest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import csr.cli.GameSession;
 
 @Component(value="GameController")
+@RequestMapping("/game")
 @RestController
 public class GameController {
 
-    @GetMapping("/game/{id}")
+    @GetMapping("/{id}")
     public void getGame(@PathVariable String id, GameSession gameSession) {
         //TODO - placeholder
     }
